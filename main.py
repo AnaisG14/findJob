d980d1cbb2d9b4f6665fe3f360d6ed438e512390
anais <gatard.anais@gmail.com>
2025-03-04 10:03:32 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+def main():
+    # Donner les informations d'une offre à un bot telegram
+    # Puis les récupérer en json pour les stocker dans une bdd sqlite (passer peut-êtr par un modèle)
+    #   - Entreprise, intitulé du post, description des missions, compétences requises
+    #   - Type de contrat, date de l'offre, lien vers l'offre
+    # Passer les informations d'une offre à chatgpt et lui demander des informations pertinantes sur l'entreprise pour répondre à cette offre
+    # Récupérer ces informations en json puis les stocker dans la bbd sqlite
+
+    # Demander au bot qui enverra à chatgpt un message personnalisé pour répondre à l'offre
+    # Stocker le message dans la bdd
+
+    # Demander au bot de mettre l'offre comme inintéressante et stocker dans la bdd
+
+    # Demander au bot de mettre l'offre comme postulé avec la date si besoin
+
+    # Pouvoir demander au bot d'afficher une offre et/ou le message pour postuler
+
+    # Avoir un fichier google sheet toujours disponible avec toutes les informations
+    return
+
+
+if __name__ == "__main__":
+    main()
