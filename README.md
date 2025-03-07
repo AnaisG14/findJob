695e7dab5fbe71819900529140f68eae119e7570
anais <gatard.anais@gmail.com>
2025-03-07 10:01:56 +0100

    bot opérationnel

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # findJob
-Utilisation d'un bot télégramme et de chatgpt pour répondre à des offres d'emploi et les suivre ensuite
+Utilisation d'un bot télégramme et de gemini pour répondre à des offres d'emploi et les suivre ensuite via un fichier google sheet
--- main.py
+++ main.py
@@ -2,25 +2,7 @@ from src.bot.bot import main_bot
 
 
 def main():
-    print('cest parti')
-    # Donner les informations d'une offre à un bot telegram
     main_bot()
-    # Puis les récupérer sous forme de liste pour les stocker dans un google sheet
-    #   - Entreprise, intitulé du post, description des missions, compétences requises
-    #   - Type de contrat, date de l'offre, lien vers l'offre
-    # Passer les informations d'une offre à chatgpt et lui demander des informations pertinantes sur l'entreprise pour
-    # répondre à cette offre
-
-    # Demander au bot qui enverra à chatgpt un message personnalisé pour répondre à l'offre
-    # Stocker le message dans la bdd
-
-    # Demander au bot de mettre l'offre comme inintéressante et stocker dans la bdd
-
-    # Demander au bot de mettre l'offre comme postulé avec la date si besoin
-
-    # Pouvoir demander au bot d'afficher une offre et/ou le message pour postuler
-
-    # Avoir un fichier google sheet toujours disponible avec toutes les informations
 
 
 if __name__ == "__main__":
